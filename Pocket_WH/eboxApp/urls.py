89a1eaeb28661d108b500bd2a694453a4ba20355
TonoLama <alama@prueba.cl>
2023-02-16 11:59:22 -0300

    Creada Pagina inicio

--- Pocket_WH/eboxApp/urls.py
+++ Pocket_WH/eboxApp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from eboxApp import views
 
 urlpatterns = [
-    path('', views.index, name = 'inicio'),
+    path('', views.index, name = 'index'),
 ]
--- Pocket_WH/eboxApp/views.py
+++ Pocket_WH/eboxApp/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from django.http import HttpResponse
+from eboxApp.models import *
+from eboxApp.forms import *
 
-def index():
-    pass
+def index(request):
+    return render (request, 'eboxApp/index.html')
