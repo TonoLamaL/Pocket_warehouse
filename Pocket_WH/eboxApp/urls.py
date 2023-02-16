c2542f3813d5dce0697e3c7c656225bb0c0551e3
TonoLama <alama@prueba.cl>
2023-02-16 15:33:17 -0300

    Creado formulario para maestra y busqueda

--- Pocket_WH/eboxApp/urls.py
+++ Pocket_WH/eboxApp/urls.py
@@ -3,5 +3,7 @@ from eboxApp import views
 
 urlpatterns = [
     path('', views.index, name = 'Index'),
-    path('maestra/', views.maestra, name = 'Maestra')
+    path('maestra/', views.maestra, name = 'Maestra'),
+    path('maestra/wMaestra_list/', views.buscarProducto, name = 'buscarProducto'),
+    path('maestra/wMaestra_full/', views.buscarMaestra, name = 'buscarTodo'),
 ]
--- Pocket_WH/eboxApp/views.py
+++ Pocket_WH/eboxApp/views.py
@@ -17,9 +17,23 @@ def maestra(request):
             informacion = maestra_prod.cleaned_data
             nuevo_producto = Maestra(nombre_producto = informacion['nombre_producto'], numero_sku = informacion['numero_sku'], categoria = informacion['categoria']) # son los argumentos de la clase y la guardo en el objeto     
             nuevo_producto.save()
-            return render(request, 'eboxApp/windows.html') # una vez que se guardo que retorne a la pagina de confrimación, y desde la confirmacion window hice un html que me retorna a inicio
+            return render(request, 'eboxApp/windowsConfirm.html') # una vez que se guardo que retorne a la pagina de confrimación, y desde la confirmacion window hice un html que me retorna a inicio
     
     else:
         maestra_prod = MaestraForm()
     
     return render(request, 'eboxApp/maestra.html', {'maestra_prod':maestra_prod}) # si hay campos vacios estamos en esta html -> este link dice quien toma la funcion de form 
+
+
+def buscarProducto(request):
+    if request.method == 'GET': # si existe un post
+        busqueda = request.GET['nombre_producto']
+        producto = Maestra.objects.filter(nombre_producto = busqueda)
+        
+        return render(request, 'eboxApp/wMaestra_list.html', {'producto':producto , 'busqueda':busqueda })
+    
+def buscarMaestra(request):
+    if request.method == 'GET': # si existe un post
+        producto = Maestra.objects.all()
+        
+        return render(request, 'eboxApp/wMaestra_full.html', {'producto':producto })
